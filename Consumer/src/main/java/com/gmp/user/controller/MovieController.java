2d8f3dc8e1f4a560dab38ce6d78e4c548e69f474
mingpenggaopc <337578303@qq.com>
2017-12-18 19:06:52 +0800

    consumer service

---
+++ Consumer/src/main/java/com/gmp/user/controller/MovieController.java
@@ -0,0 +1,23 @@
+package com.gmp.user.controller;
+
+import com.gmp.user.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class MovieController {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Value("${Url}")
+     private  String Url;
+    @GetMapping("/user/{id}")
+    public User findById(@PathVariable int  id){
+        return this.restTemplate.getForObject(Url+id,User.class);
+    }
+}
