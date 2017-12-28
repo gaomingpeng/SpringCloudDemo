6408099ceaa6c86d862191fed1fda1a545d76cb7
gaomp <337578303@qq.com>
2017-12-28 09:06:11 +0800

    增加了自定义负载均衡规则

--- Consumer/src/main/java/com/gmp/user/ConsumerApplication.java
+++ Consumer/src/main/java/com/gmp/user/ConsumerApplication.java
@@ -3,6 +3,7 @@ package com.gmp.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -10,6 +11,7 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 public class ConsumerApplication {
     @Bean
+    @LoadBalanced//此注解可以使此bean具备负载均衡的能力
     public RestTemplate restTemplate(){
         return  new RestTemplate();
     }
--- Consumer/src/main/java/com/gmp/user/controller/MovieController.java
+++ Consumer/src/main/java/com/gmp/user/controller/MovieController.java
@@ -1,24 +1,56 @@
 package com.gmp.user.controller;
 
 import com.gmp.user.entity.User;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import java.util.List;
 
 @RestController
 public class MovieController {
 
+    private  static  final Logger LOGGER = LoggerFactory.getLogger(MovieController.class);
+
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @Autowired
+    private LoadBalancerClient loadBalancerClient;
+
     @Value("${Url}")
      private  String Url;
+
+    @Value("${applicationName}")
+    private  String  applicationName;
+
+   // @GetMapping( value = "/user/{id}", produces = { "application/json;charset=UTF-8" })
     @GetMapping("/user/{id}")
     public User findById(@PathVariable int  id){
-
         return this.restTemplate.getForObject(Url+id,User.class);
+    }
+    @GetMapping("/loginstance")
+    public void logUserInstance(){
+        ServiceInstance serviceInstance = this.loadBalancerClient.choose(applicationName);
+        //打印当前选择哪个节点
+       LOGGER.info("服务名:"+serviceInstance.getServiceId());
+        LOGGER.info("IP:"+serviceInstance.getHost());
+        LOGGER.info("端口号:"+serviceInstance.getPort());
+    }
+    @GetMapping("/provider")
+    public List<ServiceInstance>  showInfo(){
+        return  this.discoveryClient.getInstances("provider");
     }
 }
