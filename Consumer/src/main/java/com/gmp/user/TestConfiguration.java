0d8fc4d87d934bc4993be2f216981b7a2fe02bf8
gaomp <337578303@qq.com>
2017-12-28 10:10:34 +0800

    注释掉了代码中配置ribbon负载均衡规则的配置，改在application.properties中配置

--- Consumer/src/main/java/com/gmp/user/TestConfiguration.java
+++ Consumer/src/main/java/com/gmp/user/TestConfiguration.java
@@ -4,7 +4,7 @@ import com.gmp.ribboncfg.RibbonConfiguration;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@RibbonClient(name="provider",configuration = RibbonConfiguration.class)
+//@Configuration
+//@RibbonClient(name="provider",configuration = RibbonConfiguration.class)
 public class TestConfiguration {
 }
