f28a0abe39398abe625cefe915678415b79c447d
gaomp <337578303@qq.com>
2018-01-02 14:15:01 +0800

    微调了pom文件

--- Provider1/src/main/java/com/gmp/user/Provider1Application.java
+++ Provider1/src/main/java/com/gmp/user/Provider1Application.java
@@ -5,8 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@EnableEurekaClient//声明这是一个eureka client
+
 @SpringBootApplication
+@EnableEurekaClient
 @MapperScan("com.gmp.*.mapper")//开启自动扫描mapper文件
 public class Provider1Application {
     public static void main(String[] args) {
